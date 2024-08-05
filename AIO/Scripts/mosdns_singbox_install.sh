8ca3cd87a8834c3ce2d29d02bc68bc30dc63ae3b
feiye2021 <79434311+feiye2021@users.noreply.github.com>
2024-08-05 10:29:54 +0800

    更新脚本

--- AIO/Scripts/mosdns_singbox_install.sh
+++ AIO/Scripts/mosdns_singbox_install.sh
@@ -29,8 +29,8 @@ home() {
             ;;
         2)
             # hostname_choose
-            wget -q -O hostname_setting.sh https://raw.githubusercontent.com/feiye2021/LinuxScripts/main/AIO/Scripts/hostname_setting.sh && chmod +x hostname_setting.sh && ./hostname_setting.sh
             echo "正在生成脚本，速度与网速有关，请等待..."
+            wget -q -O hostname_setting.sh https://raw.githubusercontent.com/feiye2021/LinuxScripts/main/AIO/Scripts/hostname_setting.sh && chmod +x hostname_setting.sh && ./hostname_setting.sh
             ;;
         3)
             basic_choose
