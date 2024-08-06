c99fde79a0211667cb2227ad2e3160a4a663c3c2
feiye2021 <79434311+feiye2021@users.noreply.github.com>
2024-08-06 11:46:16 +0800

    更新脚本

--- AIO/Scripts/singbox.sh
+++ AIO/Scripts/singbox.sh
@@ -396,7 +396,6 @@ fi
     fi
     sed -i "s|ip_cidr_ip|${ip}|g" /root/go_home.json
     sed -i "s/dns_domain/${domain}/g" /root/go_home.json
-    # sed -i "s/ip_cidr_ip/${ip}/g" /root/go_home.json
     sed -i "s/singbox_domain/${domain}/g" /root/go_home.json
     sed -i "s/singbox_hyport/${hyport}/g" /root/go_home.json
     sed -i "s/singbox_password/${password}/g" /root/go_home.json
