75b3ebf04bddc807cad7deab8596f2efda891d96
feiye2021 <79434311+feiye2021@users.noreply.github.com>
2024-08-06 15:28:19 +0800

    更新脚本

---
+++ AIO/Scripts/docker.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+clear
+rm -rf /mnt/main_install.sh
+
+################################ IP 选择 ################################
+docker_choose() {
+    clear
+    rm -rf /mnt/main_install.sh
+    echo "=================================================================="
+    echo -e "\t\tDocker综合脚本 by 忧郁滴飞叶"
+    echo -e "\t\n"
+    echo "请选择要设备的网络环境："
+    echo "=================================================================="
+    echo "1. 安装"
+    echo "2. 卸载"
+    echo "3. 安装docker-compose"
+    echo "4. 卸载docker-compose"
+    echo "5. 设定docker日志文件大小"
+    echo -e "\t"
+    echo "-. 返回上级菜单"    
+    echo "0. 退出脚本"
+    read -p "输入选项： " choice
+    case $choice in
+        1)
+            basic_settings
+            docker_install
+            ;;
+        2)
+            del_docker
+            ;;
+        3)
+            basic_settings
+            docker_compose_install
+            ;;
+        4)
+            del_docker_compose
+            ;;
+        5)
+            docker_log_setting
+            ;;
+        6)
+            docker_IPV6
+            ;;              
+        0)
+            echo -e "\e[31m退出脚本，感谢使用.\e[0m"
+            rm -rf /mnt/docker.sh    #delete         
+            ;;
+        -)
+            echo "脚本切换中，请等待..."
+            rm -rf /mnt/docker.sh    #delete
+            wget -q -O /mnt/main_install.sh https://raw.githubusercontent.com/feiye2021/LinuxScripts/main/AIO/Scripts/main_install.sh && chmod +x /mnt/main_install.sh && /mnt/main_install.sh
+            ;;
+        *)
+            echo "无效的选项，1秒后返回当前菜单，请重新选择有效的选项."
+            sleep 1
+            /mnt/docker.sh
+            ;;
+    esac 
+}
+################################ 基础环境设置 ################################
+basic_settings() {
+    echo -e "配置基础设置并安装依赖..."
+    sleep 1
+    apt update -y || { echo "\n\e[1m\e[37m\e[41m环境更新失败！退出脚本\e[0m\n"; exit 1; }
+    apt -y upgrade || { echo "\n\e[1m\e[37m\e[41m环境更新失败！退出脚本\e[0m\n"; exit 1; }
+    echo -e "\n\e[1m\e[37m\e[42m环境更新成功\e[0m\n"
+    echo -e "环境依赖安装开始..."
+    apt install wget curl vim -y || { echo -e "\n\e[1m\e[37m\e[41m环境依赖安装失败！退出脚本\e[0m\n"; exit 1; }
+    echo -e "\n\e[1m\e[37m\e[42mmosdns依赖安装成功\e[0m\n"
+    timedatectl set-timezone Asia/Shanghai || { echo -e "\n\e[1m\e[37m\e[41m时区设置失败！退出脚本\e[0m\n"; exit 1; }
+}
+################################ docker安装 ################################
+docker_install() {
+    echo -e "开始安装docker..." 
+    wget -qO- get.docker.com | bash
+    systemctl enable docker --now
+    if ! systemctl is-active --quiet docker; then
+    echo -e "\n\e[1m\e[37m\e[41mdocker安装失败！退出脚本\e[0m\n"
+    rm -rf /mnt/docker.sh    #delete    
+    exit 1
+    fi
+    systemctl restart docker
+    rm -rf /mnt/docker.sh    #delete      
+    echo "=================================================================="
+    echo -e "\t\tDocker 安装完成"
+    echo -e "\n"
+    echo -e "温馨提示:\n本脚本仅在 ubuntu22.04 环境下测试，其他环境未经验证，已查\n询程序运行状态，如出现\e[1m\e[32m active (running)\e[0m，程序已启动成功。"
+    echo "=================================================================="
+    systemctl status docker
+}
+################################ docker卸载 ################################
+del_docker() {
+    echo -e "开始卸载docker..."     
+    apt remove docker docker-engine
+    rm -rf /var/lib/docker/
+    rm -rf /mnt/docker.sh    #delete         
+    echo -e "\n\e[1m\e[37m\e[42mdocker卸载成功\e[0m\n"
+}
+################################ docker-compose安装 ################################
+docker_compose_install() {
+    echo -e "开始安装docker-compose..."
+    # Compose_Version=$(curl -s https://github.com/docker/compose/releases | grep '/releases/tag/v' | head -n 1 | awk -F'/releases/tag/' '{print $2}' | awk -F'"' '{print $1}')
+    # curl -L "https://github.com/docker/compose/releases/download/${Compose_Version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    curl -L "https://github.com/docker/compose/releases/download/v2.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    if docker-compose --version | grep -q '^Docker Compose version v[0-9]\+\.[0-9]\+\.[0-9]\+$'; then
+        docker-compose --version
+    else
+        echo -e "\n\e[1m\e[37m\e[41mdocker安装失败！退出脚本\e[0m\n"
+        rm -rf /mnt/docker.sh    #delete    
+        exit 1
+    fi
+    rm -rf /mnt/docker.sh    #delete      
+    echo "=================================================================="
+    echo -e "\t\tDocker-Compose 安装完成"
+    echo -e "\n"
+    echo -e "温馨提示:\n本脚本仅在 ubuntu22.04 环境下测试，其他环境未经验证"
+    echo "=================================================================="
+    systemctl restart docker
+}
+################################ docker-compose卸载 ################################
+del_docker() {
+    echo -e "开始卸载docker-compose..."     
+    rm -rf /usr/local/bin/docker-compose
+    rm -rf /mnt/docker.sh    #delete         
+    echo -e "\n\e[1m\e[37m\e[42mdocker-compose卸载成功\e[0m\n"
+}
+################################ 设定docker日志文件大小 ################################
+docker_log_setting() {
+    while true; do
+        read -p "请输入日志文件的最大大小（单位m，例如：20、50等，默认50）： " LOG_SIZE
+        LOG_SIZE="${LOG_SIZE:-50}"
+        if [[ $LOG_SIZE =~ ^[0-9]+$ ]]; then
+            break
+        else
+            echo -e "\e[31m日志文件的大小格式输入不正确，请重新输入\e[0m"
+        fi
+    done
+    # 创建临时文件用于日志设置
+    TMP_FILE=$(mktemp)
+    # 将用户输入的数值与固定的单位 "m" 写入临时文件
+# 将用户输入的数值与固定的单位 "m" 写入临时文件
+cat > $TMP_FILE <<EOF
+{
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "${LOG_SIZE}m",
+        "max-file": "3"
+    }
+}
+EOF
+    # 合并日志设置与现有的 daemon.json
+    jq -s '.[0] * .[1]' /etc/docker/daemon.json $TMP_FILE | sudo tee /etc/docker/daemon.json > /dev/null
+    # 清理临时文件
+    rm $TMP_FILE
+    # 重新启动 Docker 服务以应用新的设置
+    sudo systemctl restart docker
+    rm -rf /mnt/docker.sh    #delete   
+    echo "Docker 日志设置已更新，最大日志文件大小为 ${LOG_SIZE}m"
+
+}
+################################ 开启docker IPV6 ################################
+docker_IPV6() {
+    # 创建临时文件用于 IPv6 设置
+    TMP_FILE=$(mktemp)
+    # 将 IPv6 设置写入临时文件
+cat > $TMP_FILE <<EOF
+{
+    "ipv6": true,
+    "fixed-cidr-v6": "fd00:dead:beef:c0::/80",
+    "experimental": true,
+    "ip6tables": true
+}
+EOF
+    # 合并 IPv6 设置与现有的 daemon.json
+    jq -s '.[0] * .[1]' /etc/docker/daemon.json $TMP_FILE | sudo tee /etc/docker/daemon.json > /dev/null
+    # 清理临时文件
+    rm $TMP_FILE
+    # 重新启动 Docker 服务以应用新的设置
+    sudo systemctl restart docker
+    rm -rf /mnt/docker.sh    #delete 
+    echo "Docker IPv6 设置已更新"
+}
+################################ docker卸载 ################################
+del_docker() {
+    echo -e "开始卸载docker..."     
+    apt remove docker docker-engine
+    rm -rf /var/lib/docker/
+    rm -rf /mnt/docker.sh    #delete         
+    echo -e "\n\e[1m\e[37m\e[42mdocker卸载成功\e[0m\n"
+}
+################################ 主程序 ################################
+docker_choose
