5716d4a4614c429464e25d31276ef70b7e60af14
feiye2021 <79434311+feiye2021@users.noreply.github.com>
2024-08-07 16:11:50 +0800

    新增增加/删除SWAP

--- AIO/Scripts/basic_settings.sh
+++ AIO/Scripts/basic_settings.sh
@@ -64,6 +64,9 @@ basic_choose() {
             modify_dns_stub_listener
             rm -rf /mnt/basic_settings.sh    #delete                   
             ;;
+        7)            
+            swap_choose
+            ;;
         0)
             red "退出脚本，感谢使用."
             rm -rf /mnt/basic_settings.sh    #delete         
@@ -113,5 +116,91 @@ modify_dns_stub_listener() {
     systemctl restart systemd-resolved.service || { red "重启 systemd-resolved.service 失败！退出脚本"; exit 1; }
     green "关闭53端口监听成功"
 }
+################################ 添加SWAP ################################
+add_swap(){
+    white "请输入需要添加的swap，建议为${yellow}内存的2倍${reset}！"
+    while true; do
+        read -p "请输入swap数值:" swapsize
+        if [[ $swapsize =~ ^[0-9]+$ ]]; then
+            break
+        else
+            red "swap数值格式不正确，请重新输入"
+        fi
+    done
+    #检查是否存在swapfile
+    grep -q "swapfile" /etc/fstab
+    #如果不存在将为其创建swap
+    if [ $? -ne 0 ]; then
+        white "swapfile未发现，正在为其创建swapfile"
+        fallocate -l ${swapsize}M /swapfile
+        chmod 600 /swapfile
+        mkswap /swapfile
+        swapon /swapfile
+        echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+        green "swap创建成功，并查看信息："
+        cat /proc/swaps
+        cat /proc/meminfo | grep Swap
+        rm -rf /mnt/basic_settings.sh    #delete     
+    else
+        red "swapfile已存在，swap设置失败，请先运行脚本删除swap后重新设置！"
+        rm -rf /mnt/basic_settings.sh    #delete
+        exit 1
+    fi
+}
+################################ 删除SWAP ################################
+del_swap(){
+#检查是否存在swapfile
+    grep -q "swapfile" /etc/fstab
+    #如果存在就将其移除
+    if [ $? -eq 0 ]; then
+        white "swapfile已发现，正在将其移除..."
+        sed -i '/swapfile/d' /etc/fstab
+        echo "3" > /proc/sys/vm/drop_caches
+        swapoff -a
+        rm -f /swapfile
+        green "swap已删除！"
+        rm -rf /mnt/basic_settings.sh    #delete 
+    else
+        red "swapfile未发现，swap删除失败！"
+        rm -rf /mnt/basic_settings.sh    #delete
+        exit 1 
+    fi
+}
+################################ SWAP选择 ################################
+swap_choose(){
+    clear
+    echo "=================================================================="
+    echo -e "\t\t SWAP 添加/删除脚本 by 忧郁滴飞叶"
+    echo -e "\t\n"   
+    echo "请选择要操作的基本设置："
+    echo "=================================================================="   
+    echo "1. 添加swap"
+    echo "2. 删除swap"
+    echo -e "\t" 
+    echo "-. 返回上级菜单"      
+    echo "0. 退出脚本"         
+    read -p "请选择服务: " num
+    case "$num" in
+        1)
+        add_swap
+        ;;
+        2)
+        del_swap
+        ;;
+        -)
+        white "脚本切换中，请等待..."
+        basic_choose
+        ;;
+        0)
+        red "退出脚本，感谢使用."
+        rm -rf /mnt/basic_settings.sh    #delete         
+        ;;
+        *)
+        white "无效的选项，1秒后返回当前菜单，请重新选择有效的选项."
+        sleep 1
+        swap_choose
+        ;;
+        esac
+    }
 ################################ 主程序 ################################
 basic_choose
