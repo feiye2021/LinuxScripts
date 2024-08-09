6c323944e0d080b6ad0265aebe32ea700fb13373
feiye2021 <79434311+feiye2021@users.noreply.github.com>
2024-08-09 16:28:49 +0800

    新增docker compose脚本

--- AIO/Scripts/main_install.sh
+++ AIO/Scripts/main_install.sh
@@ -29,6 +29,7 @@ main() {
     echo "5. Sing-box"
     echo "6. PVE系列"       
     echo "7. Docker"
+    echo "8. Docker-Compose配置生成"
     echo -e "\t"    
     echo "0. 退出脚本"        
     read -p "请选择服务: " choice
@@ -61,6 +62,10 @@ main() {
             white "脚本切换中，请等待..."
             wget -q -O /mnt/docker.sh https://raw.githubusercontent.com/feiye2021/LinuxScripts/main/AIO/Scripts/docker.sh && chmod +x /mnt/docker.sh && /mnt/docker.sh
             ;;
+        7)
+            white "脚本切换中，请等待..."
+            wget -q -O /mnt/docker_compose.sh https://raw.githubusercontent.com/feiye2021/LinuxScripts/main/AIO/Scripts/docker_compose.sh && chmod +x /mnt/docker_compose.sh && /mnt/docker_compose.sh
+            ;;            
         0)
             red "退出脚本，感谢使用."
             rm -rf /mnt/main_install.sh
