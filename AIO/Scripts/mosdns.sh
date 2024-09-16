d94266b6a183901e412be9239c257c4a672d9285
feiye2021 <79434311+feiye2021@users.noreply.github.com>
2024-09-16 21:23:37 +0800

    代码更新，新增UI版本选择

--- AIO/Scripts/mosdns.sh
+++ AIO/Scripts/mosdns.sh
@@ -32,11 +32,11 @@ mosdns_choose() {
     echo "1. 安装Mosdns"
     echo "2. 更新Mosdns"
     echo "3. 重置Mosdns缓存"    
-    echo "4. 安装Mosdns UI"
+    echo "4. 安装Mosdns UI（版本选择）"
     echo "5. 卸载Mosdns"
     echo "6. 卸载Mosdns UI"
     echo -e "\t"
-    echo "8. 一键安装Mosdns及UI面板"
+    echo "8. 一键安装Mosdns及UI面板（版本选择）"
     echo "9. 一键卸载Mosdns及UI面板"
     echo "-. 返回上级菜单"          
     echo "0. 退出脚本"        
@@ -55,8 +55,8 @@ mosdns_choose() {
             del_mosdns_cache || exit 1
             ;;        
         4)
-            white "安装Mosdns UI"
-            install_mosdns_ui
+            white "\n\t\t\t安装Mosdns UI\n"
+            install_mosdns_ui_choose_version
             ;;
         5)
             white "卸载Mosdns"
@@ -69,8 +69,8 @@ mosdns_choose() {
             rm -rf /mnt/mosdns.sh    #delete                 
             ;;
         8)
-            white "一键安装Mosdns及UI面板"
-            install_mosdns_ui_all
+            white "\n\t\t\t一键安装Mosdns及UI面板\n"
+            install_mosdns_ui_all_chose_version
             ;;
         9)
             white "一键卸载Mosdns及UI面板"
@@ -90,7 +90,7 @@ mosdns_choose() {
         *)
             white "无效的选项，1秒后返回当前菜单，请重新选择有效的选项."
             sleep 1
-            /mnt/mosdns.sh
+            mosdns_choose
             ;;
     esac
 }
@@ -106,7 +106,46 @@ install_mosdns() {
     enable_autostart || exit 1
     install_complete
 }
+
+################################ MosDNS UI版本选择 ################################
+install_mosdns_ui_choose_version() {
+    while true; do
+        white "两个版本 UI 版本样式无差别，区别仅在提供方案大佬不同，选择需要安装的MosDNS UI版本："
+        white "1. ${yellow}孔昊天${reset}（孔大版）"
+        white "2. ${yellow}Οὐρανός${reset}（O佬版）"
+        read -p "请输入需安装的MosDNS UI版本（默认1）: " choose_version_for_ui
+        choose_version_for_ui="${choose_version_for_ui:-1}"
+        if [[ $choose_version_for_ui =~ ^[1-2]$ ]]; then
+            break
+        else
+            red "输入选定的版本数字不正确，请重新输入"
+        fi
+    done
+    case $choose_version_for_ui in
+        1) install_mosdns_ui_TO_kong_hao_tian ;;
+        2) install_mosdns_ui_TO_Ovpavac ;;
+    esac
+}
 ################################ MosDNS及UI一键 ################################
+install_mosdns_ui_all_chose_version() {
+    while true; do
+        white "两个版本 UI 版本样式无差别，区别仅在提供方案大佬不同，选择需要安装的MosDNS UI版本："
+        white "1. ${yellow}孔昊天${reset}（孔大版）"
+        white "2. ${yellow}Οὐρανός${reset}（O佬版）"
+        read -p "请输入需安装的MosDNS UI版本（默认1）: " choose_version_for_all
+        choose_version_for_all="${choose_version_for_all:-1}"
+        if [[ $choose_version_for_all =~ ^[1-2]$ ]]; then
+            break
+        else
+            red "输入选定的版本数字不正确，请重新输入"
+        fi
+    done
+    case $choose_version_for_all in
+        1) install_mosdns_ui_all "kong_hao_tian" ;;
+        2) install_mosdns_ui_all "Ovpavac" ;;
+    esac
+}
+
 install_mosdns_ui_all() {
     white "开始安装MosDNS ..."   
     mkdir /mnt/mosdns && cd /mnt/mosdns
@@ -118,13 +157,31 @@ install_mosdns_ui_all() {
     configure_mosdns || exit 1
     enable_autostart || exit 1
     systemctl restart mosdns
-    white "开始安装MosDNS UI..."    
+    Web_ui_version="$1"
+    if [[ "$Web_ui_version" == "kong_hao_tian" ]]; then
+        kong_hao_tian
+    elif [[ "$Web_ui_version" == "Ovpavac" ]]; then
+        Ovpavac
+    fi
+}
+
+kong_hao_tian() {
+    white "开始安装MosDNS UI (${yellow}孔昊天版${reset}) ..."   
     install_loki || exit 1
     install_vector || exit 1
     install_prometheus || exit 1
     install_grafana || exit 1
     install_complete_all
 }
+
+Ovpavac() {
+    white "开始安装MosDNS UI (${yellow}Οὐρανός版${reset}) ..."    
+    loki_install_Ovpavac
+    vector_install_Ovpavac
+    prometheus_install_Ovpavac
+    grafana_install_Ovpavac
+    ui_install_complete_all_Ovpavac
+}   
 ################################用户自定义设置################################
 mosdns_customize_settings() {
     echo -e "\n自定义设置（以下设置可直接回车使用默认值）"
@@ -232,8 +289,8 @@ del_mosdns_cache() {
     green "Mosdns缓存已重置"
 }
 ################################ Mosdns UI安装 ################################
-install_mosdns_ui() {
-    echo "开始安装MosDNS UI..."    
+install_mosdns_ui_TO_kong_hao_tian() {
+    white "开始安装MosDNS UI (${yellow}孔昊天版${reset}) ..."    
     basic_settings || exit 1
     install_loki || exit 1
     install_vector || exit 1
@@ -241,7 +298,16 @@ install_mosdns_ui() {
     install_grafana || exit 1
     install_complete_ui
 }
-################################ Loki 安装 ################################
+install_mosdns_ui_TO_Ovpavac() {
+    white "开始安装MosDNS UI (${yellow}Οὐρανός版${reset}) ..."    
+    basic_settings || exit 1
+    loki_install_Ovpavac
+    vector_install_Ovpavac
+    prometheus_install_Ovpavac
+    grafana_install_Ovpavac
+    install_complete_ui_Ovpavac
+}
+################################ Loki 安装（孔昊天） ################################
 install_loki() {
     white "开始安装Loki..."
     mkdir /mnt/ui && cd /mnt/ui
@@ -250,7 +316,7 @@ install_loki() {
     systemctl enable loki --now
     green "Loki已安装完成"
 }
-################################ Vector 安装 ################################
+################################ Vector 安装（孔昊天） ################################
 install_vector() {
     white "开始安装Vector..."
     cd /mnt/ui
@@ -280,7 +346,7 @@ EOF
     sudo systemctl enable vector --now
     green "Vector已安装完成"
 }
-################################ Prometheus 安装 ################################
+################################ Prometheus 安装（孔昊天） ################################
 install_prometheus() {
     white "开始安装Prometheus..."
     sudo apt-get install -y prometheus
@@ -296,7 +362,7 @@ EOF
     sudo systemctl restart prometheus
     green "Prometheus已安装完成"
 }
-################################ Grafana 安装 ################################
+################################ Grafana 安装（孔昊天） ################################
 install_grafana() {
     white "开始安装Grafana..."
     cd /mnt/ui
@@ -313,6 +379,123 @@ install_grafana() {
         rm -rf /mnt/mosdns.sh    #delete  
         red "Grafana安装失败或未能启动" || exit 1
     fi
+}
+################################ Loki 安装（Ovpavac） ################################
+loki_install_Ovpavac() {
+    white "开始下载并安装loki..."
+    [ ! -d "/mnt/mosdnsui/loki" ] && mkdir -p "/mnt/mosdnsui/loki"
+    cd /mnt/mosdnsui/loki
+    wget --quiet --show-progress https://raw.githubusercontent.com/grafana/loki/v3.1.0/cmd/loki/loki-local-config.yaml
+    wget --quiet --show-progress https://github.com/grafana/loki/releases/download/v3.1.0/loki_3.1.0_amd64.deb
+
+    dpkg -i loki_3.1.0_amd64.deb
+    systemctl daemon-reload
+    systemctl enable loki --now
+
+    white "\n正在检查 ${yellow}loki${reset} 安装状态，请稍后..."
+    if ! command -v loki >/dev/null 2>&1; then
+        red "Loxki 安装失败，请重新运行脚本"
+        rm -rf /mnt/mosdns.sh    # 删除脚本
+        exit 1
+    fi
+
+    green "loki 安装成功"
+}
+################################ Vector 安装（Ovpavac） ################################
+vector_install_Ovpavac() {
+    white "开始下载并安装vector..."
+    bash -c "$(curl -L https://setup.vector.dev)"
+    apt install vector -y
+    
+    if [ -d "/etc/vector/cache" ]; then
+        chmod -R 777 /etc/vector/cache
+    else
+        install -d -m 777 /etc/vector/cache
+    fi
+
+    # 备份并清空 vector.yaml 文件
+    [ -f "/etc/vector/vector.yaml" ] && cp "/etc/vector/vector.yaml" "/etc/vector/vector.yaml.bak_$(date +%F_%T)" && > "/etc/vector/vector.yaml"
+
+    wget --quiet --show-progress -O /etc/vector/vector.yaml https://raw.githubusercontent.com/feiye2021/LinuxScripts/main/AIO/Configs/mosdns/vector.yaml
+    sed -i "s|/tmp/vector|/etc/vector/cache|g" /etc/vector/vector.yaml
+
+    systemctl daemon-reload
+    systemctl enable vector --now
+
+    white "\n正在检查 ${yellow}vector${reset} 安装状态，请稍后..."
+    if ! command -v vector >/dev/null 2>&1; then
+        red "Vector 安装失败，请重新运行脚本"
+        rm -rf /mnt/mosdns.sh    # 删除脚本
+        exit 1
+    fi
+    systemctl daemon-reload
+    systemctl restart vector.service
+    green "vector 安装成功"
+}
+################################ Prometheus 安装（Ovpavac） ################################
+prometheus_install_Ovpavac() {
+    white "开始下载并安装prometheus..."
+    [ ! -d "/mnt/mosdnsui/prometheus" ] && mkdir -p "/mnt/mosdnsui/prometheus"
+    cd /mnt/mosdnsui/prometheus
+    wget --quiet --show-progress https://github.com/prometheus/prometheus/releases/download/v2.53.0/prometheus-2.53.0.linux-amd64.tar.gz
+
+    tar zxf prometheus-2.53.0.linux-amd64.tar.gz -C /mnt/mosdnsui/prometheus
+    mv /mnt/mosdnsui/prometheus/prometheus-2.53.0.linux-amd64 /etc/prometheus
+
+    # 备份并清空 prometheus.service 文件
+    [ -f "/usr/lib/systemd/system/prometheus.service" ] && cp "/usr/lib/systemd/system/prometheus.service" "/usr/lib/systemd/system/prometheus.service.bak_$(date +%F_%T)" && > "/usr/lib/systemd/system/prometheus.service"
+
+    # 写入新的 prometheus.service 配置
+    cat <<EOF > /usr/lib/systemd/system/prometheus.service
+[Unit]
+Description=prometheus service
+[Service]
+User=root
+ExecStart=/etc/prometheus/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/etc/prometheus/data
+TimeoutStopSec=10
+Restart=on-failure
+RestartSec=5
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    # 备份并清空 prometheus.yml 文件
+    [ -f "/etc/prometheus/prometheus.yml" ] && cp "/etc/prometheus/prometheus.yml" "/etc/prometheus/prometheus.yml.bak_$(date +%F_%T)" && > "/etc/prometheus/prometheus.yml"
+ 
+    wget --quiet --show-progress -O /etc/prometheus/prometheus.yml https://raw.githubusercontent.com/feiye2021/LinuxScripts/main/AIO/Configs/mosdns/prometheus.yml
+
+    systemctl daemon-reload
+    systemctl enable prometheus --now
+
+    white "\n正在检查 ${yellow}prometheus${reset} 安装状态，请稍后..."
+
+    if ! systemctl list-units --type=service | grep -q "prometheus.service"; then
+        red "Prometheus 安装失败，请重新运行脚本"
+        rm -rf /mnt/mosdns.sh    # 删除脚本
+        exit 1
+    fi
+
+    green "prometheus 安装成功"    
+}
+################################ Grafana 安装（Ovpavac） ################################
+grafana_install_Ovpavac() {
+    white "开始下载并安装gafana..."
+    apt-get install -y adduser libfontconfig1 musl
+    [ ! -d "/mnt/mosdnsui/gafana" ] && mkdir -p "/mnt/mosdnsui/gafana"
+    cd /mnt/mosdnsui/gafana
+    wget --quiet --show-progress https://dl.grafana.com/enterprise/release/grafana-enterprise_11.1.0_amd64.deb
+
+    dpkg -i grafana-enterprise_11.1.0_amd64.deb
+    systemctl enable grafana-server --now
+
+    white "\n正在检查 ${yellow}grafana-server${reset} 安装状态，请稍后..."
+    if ! command -v grafana-server >/dev/null 2>&1; then
+        red "Grafana-server 安装失败，请重新运行脚本"
+        rm -rf /mnt/mosdns.sh    # 删除脚本
+        exit 1
+    fi
+
+    green "grafana-server 安装成功"
 }
 ################################ 更新Mosdns ################################
 update_mosdns() {
@@ -435,12 +618,65 @@ install_complete_ui() {
     local_ip=$(hostname -I | awk '{print $1}')
     rm -rf /mnt/mosdns.sh    #delete       
 echo "=================================================================="
-echo -e "\t\tMosdns UI 安装完成"
+echo -e "\t\tMosdns UI (孔昊天版) 安装完成"
 echo -e "\n"
-echo -e "请打开：${yellow}http://$local_ip:3000${reset},进入ui管理界面，默认账号及密码均为${yellow}admin${reset}"
+echo -e "请打开：${yellow}http://$local_ip:3000${reset}\n进入ui管理界面，默认账号及密码均为：\n${yellow}admin${reset}"
 echo "=================================================================="
 }
-################################ Mosdns 一键安装结束 ################################
+
+install_complete_ui_Ovpavac() {
+    systemctl restart mosdns
+    rm -r /mnt/mosdns
+    rm -r /mnt/mosdnsui
+
+    white "\n正在检查 ${yellow}loki${reset} 运行状态中，请稍后..."
+    sleep 2
+    if ! systemctl is-active --quiet loki; then
+        red "服务 Loki 启动失败，脚本退出！"
+        rm -rf /mnt/mosdns.sh    #delete  
+        exit 1
+    fi
+    green "Loki服务运行正常"
+
+    systemctl restart vector.service
+    white "\n检查 ${yellow}vector${reset} 运行状态中，请稍后..."
+    sleep 2
+    if ! systemctl is-active --quiet vector; then
+        red "服务 Vector 启动失败，脚本退出！"
+        rm -rf /mnt/mosdns.sh    #delete  
+        exit 1
+    fi
+    green "Vector服务运行正常"
+
+    systemctl restart prometheus
+    white "\n检查 ${yellow}prometheus${reset} 运行状态中，请稍后..."
+    sleep 2
+    if ! systemctl is-active --quiet prometheus; then
+        red "服务 Prometheus 启动失败，脚本退出！"
+        rm -rf /mnt/mosdns.sh    #delete  
+        exit 1
+    fi
+    green "Prometheus服务运行正常"
+
+    systemctl restart grafana-server
+    white "\n检查 ${yellow}grafana-server${reset} 运行状态中，请稍后..."
+    sleep 2
+    if ! systemctl is-active --quiet grafana-server; then
+        red "服务 Grafana-server 启动失败，脚本退出！"
+        rm -rf /mnt/mosdns.sh    #delete  
+        exit 1
+    fi
+    green "Grafana-server服务运行正常"
+
+    local_ip=$(hostname -I | awk '{print $1}')
+    rm -rf /mnt/mosdns.sh    #delete           
+echo "=================================================================="
+echo -e "\t\tMosdns UI （Οὐρανός版） 安装完成"
+echo -e "\n"
+echo -e "请打开：${yellow}http://$local_ip:3000${reset}\n进入ui管理界面，默认账号及密码均为：\n${yellow}admin${reset}"
+echo "=================================================================="
+}
+################################ Mosdns 一键安装结束(孔昊天) ################################
 install_complete_all() {
     systemctl restart mosdns
     sudo rm -r /mnt/mosdns || exit 1
@@ -452,10 +688,66 @@ install_complete_all() {
     local_ip=$(hostname -I | awk '{print $1}')
     rm -rf /mnt/mosdns.sh    #delete       
 echo "=================================================================="
-echo -e "\t\tMosdns及UI一键安装完成"
+echo -e "\t\tMosdns及UI（孔昊天版）一键安装完成"
 echo -e "\n"
 echo -e "Mosdns运行目录为${yellow}/etc/mosdns${reset}"
-echo -e "请打开：${yellow}http://$local_ip:3000${reset},进入ui管理界面，默认账号及密码均为：\n${yellow}admin${reset}"
+echo -e "请打开：${yellow}http://$local_ip:3000${reset}\n进入ui管理界面，默认账号及密码均为：\n${yellow}admin${reset}"
+echo -e "温馨提示:\n本脚本仅在 ubuntu22.04 环境下测试，其他环境未经验证，已查\n询程序运行状态，如出现\e[1m\e[32m active (running)\e[0m，程序已启动成功。\n网关自行配置为sing-box，dns为Mosdns地址"
+echo "=================================================================="
+systemctl status mosdns
+}
+################################ Mosdns 一键安装结束(Ovpavac) ################################
+ui_install_complete_all_Ovpavac() {
+    systemctl restart mosdns
+    rm -r /mnt/mosdns
+    rm -r /mnt/mosdnsui
+
+    white "\n正在检查 ${yellow}loki${reset} 运行状态中，请稍后..."
+    sleep 2
+    if ! systemctl is-active --quiet loki; then
+        red "服务 Loki 启动失败，脚本退出！"
+        rm -rf /mnt/mosdns.sh    #delete  
+        exit 1
+    fi
+    green "Loki服务运行正常"
+
+    systemctl restart vector.service
+    white "\n检查 ${yellow}vector${reset} 运行状态中，请稍后..."
+    sleep 2
+    if ! systemctl is-active --quiet vector; then
+        red "服务 Vector 启动失败，脚本退出！"
+        rm -rf /mnt/mosdns.sh    #delete  
+        exit 1
+    fi
+    green "Vector服务运行正常"
+
+    systemctl restart prometheus
+    white "\n检查 ${yellow}prometheus${reset} 运行状态中，请稍后..."
+    sleep 2
+    if ! systemctl is-active --quiet prometheus; then
+        red "服务 Prometheus 启动失败，脚本退出！"
+        rm -rf /mnt/mosdns.sh    #delete  
+        exit 1
+    fi
+    green "Prometheus服务运行正常"
+
+    systemctl restart grafana-server
+    white "\n检查 ${yellow}grafana-server${reset} 运行状态中，请稍后..."
+    sleep 2
+    if ! systemctl is-active --quiet grafana-server; then
+        red "服务 Grafana-server 启动失败，脚本退出！"
+        rm -rf /mnt/mosdns.sh    #delete  
+        exit 1
+    fi
+    green "Grafana-server服务运行正常"
+
+    local_ip=$(hostname -I | awk '{print $1}')
+    rm -rf /mnt/mosdns.sh    #delete       
+echo "=================================================================="
+echo -e "\t\tMosdns及UI（Οὐρανός版）一键安装完成"
+echo -e "\n"
+echo -e "Mosdns运行目录为${yellow}/etc/mosdns${reset}"
+echo -e "请打开：${yellow}http://$local_ip:3000${reset}\n进入ui管理界面，默认账号及密码均为：\n${yellow}admin${reset}"
 echo -e "温馨提示:\n本脚本仅在 ubuntu22.04 环境下测试，其他环境未经验证，已查\n询程序运行状态，如出现\e[1m\e[32m active (running)\e[0m，程序已启动成功。\n网关自行配置为sing-box，dns为Mosdns地址"
 echo "=================================================================="
 systemctl status mosdns
