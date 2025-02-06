8353433ceceeb24dd082150b61d8be174eac8427
feiye2021 <79434311+feiye2021@users.noreply.github.com>
2025-02-06 14:01:27 +0800

    更新

--- AIO/Scripts/mosdns20240924.sh
+++ AIO/Scripts/mosdns20240924.sh
@@ -154,7 +154,7 @@ install_mosdns_ui_all_chose_version() {
         white "1. ${yellow}孔昊天版${reset}"
         white "2. ${yellow}Οὐρανός版${reset}"
         read -p "请输入需安装的MosDNS UI版本（默认1）: " choose_version_for_all
-        choose_version_for_all="${choose_version_for_all:-1}"
+        choose_version_for_all="${choose_version_for_all:-2}"
         if [[ $choose_version_for_all =~ ^[1-2]$ ]]; then
             break
         else
