fac9e716f541442bd6d693afcaa2a716b043e80b
FeiYe <79434311+feiye2021@users.noreply.github.com>
2025-10-16 11:56:21 +0800

    更新Mosdns的PH20251016版

--- AIO/Scripts/mosdns.sh
+++ AIO/Scripts/mosdns.sh
@@ -21,7 +21,7 @@ white(){
     echo -e "$1"
 }
 #配置版本
-mosdns_latest_version_PH=mosdns-ph-20250912
+mosdns_latest_version_PH=mosdns-ph-20251016
 mosdns_latest_version_oupavoc=20240930
 
 private_ip=$(ip route get 1.2.3.4 | awk '{print $7}' | head -1)
